b01fa1c18c7b27fb5a650f411a5f3e7f3608946a
fandablazek97 <blazek127@gmail.com>
2022-02-06 21:35:11 +0100

    initial commitgit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,22 @@
 module.exports = {
   siteMetadata: {
-      title: `Tailwindcss starter`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Tailwindcss starter`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-postcss",
+  ],
 };
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function MainPage() {
+  return (
+    <Layout>
+      <h1 className="text-3xl text-red-600 font-bold">Tailwindcss jede!!</h1>
+    </Layout>
+  );
+}
